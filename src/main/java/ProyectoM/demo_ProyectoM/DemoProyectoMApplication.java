a453b4ddf0019957d7ba16a37fde5e75f7c0f992
Vitezzz <humbertohernandezdelacruz@gmail.com>
2025-09-15 23:31:50 -0600

    Primer commit

---
+++ src/main/java/ProyectoM/demo_ProyectoM/DemoProyectoMApplication.java
@@ -0,0 +1,13 @@
+package ProyectoM.demo_ProyectoM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProyectoMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProyectoMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProyectoM/demo_ProyectoM/DemoProyectoMApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoM.demo_ProyectoM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProyectoMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
